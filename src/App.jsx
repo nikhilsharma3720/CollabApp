cfced7c4b5be7a2fb2e75800ee7275f1770f4349
Nikhil Sharma <nikhilsharma@Nikhils-MacBook-Air.local>
2026-02-02 01:44:53 +0530

    Fixing case sensitivity for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,20 @@ import { Provider, useDispatch } from "react-redux";
 import { store } from "../redux/store";
 import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
-import Navbar from "./components/Navbar.jsx";
-import SignIn from "./pages/SignIn.jsx";
-import SignUp from "./pages/SignUp.jsx";
-import Board from "./pages/Board.jsx";
-import CreateJoinTeam from "./components/CreateJoinTeam.jsx";
+
+import Navbar from "./Components/Navbar";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Board from "./pages/Board";
+import CreateJoinTeam from "./Components/Teams/createJoinTeam";
 import api from "./api/axios";
 import { setUser, clearUser } from "../redux/Slices/userSlice";
 
-// ✅ Handles Global Auth Logic
 function AuthLoader({ children }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
 
-  // Define paths that don't require a logged-in user
   const publicPaths = ["/signin", "/signup"];
 
   useEffect(() => {
@@ -32,21 +31,16 @@ function AuthLoader({ children }) {
         const res = await api.get("/me", { withCredentials: true });
         const userData = res.data;
 
-        // 1. Sync state and storage
         localStorage.setItem("user", JSON.stringify(userData));
         dispatch(setUser(userData));
 
-        // 2. 🛡️ PREVENT GOING BACK TO SIGNIN/SIGNUP
-        // If user is logged in and tries to go to /signin or /signup, send them home
         if (publicPaths.includes(location.pathname)) {
           navigate("/createAndJoin", { replace: true });
         }
       } catch (err) {
-        // 3. Handle Session Failure
         dispatch(clearUser());
         localStorage.removeItem("user");
 
-        // Only redirect to signin if they are trying to access protected content
         if (!publicPaths.includes(location.pathname)) {
           navigate("/signin", { replace: true });
         }
@@ -54,7 +48,7 @@ function AuthLoader({ children }) {
     };
 
     checkAuth();
-  }, [location.pathname]);
+  }, [location.pathname, dispatch, navigate]);
 
   return children;
 }
@@ -71,7 +65,6 @@ function App() {
             <Route path="/signup" element={<SignUp />} />
             <Route path="/boards" element={<Board />} />
             <Route path="/createAndJoin" element={<CreateJoinTeam />} />
-            {/* Default fallback: redirect to signin */}
             <Route path="*" element={<SignIn />} />
           </Routes>
         </AuthLoader>
--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
-import BoardCard from "../components/BoardCard";
+import BoardCard from "../Components/BoardCard";
 import { useSelector } from "react-redux";
 import api from "../api/axios";
 import socket from "../../socket";
